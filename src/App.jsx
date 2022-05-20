13e87f1647bbb0c8b695036c4b2a5598c8800a26
Caique <aecaique@gmail.com>
2022-05-20 18:51:22 -0300

    active state for filter__btn

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import moletom__sangue from "./assets/moletom__sangue.jpg";
 import moletom__morte from "./assets/moletom__morte.jpg";
 import moletom__energia from "./assets/moletom__energia.jpg";
 import moletom__conhecimento from "./assets/moletom__conhecimento.jpg";
-import camisa__carina from "./assets/camisa__carina.jpg";
-import camisa__ordem from "./assets/camisa__ordem.jpg";
-import camisa__joui from "./assets/camisa__joui.jpg";
-import camisa__arthur from "./assets/camisa__arthur.jpg";
-import camisa__mestre from "./assets/camisa__mestre.jpg";
+import camiseta__carina from "./assets/camiseta__carina.jpg";
+import camiseta__ordem from "./assets/camiseta__ordem.jpg";
+import camiseta__joui from "./assets/camiseta__joui.jpg";
+import camiseta__arthur from "./assets/camiseta__arthur.jpg";
+import camiseta__mestre from "./assets/camiseta__mestre.jpg";
 import mangalonga__carina from "./assets/mangalonga__carina.jpg";
 import mangalonga__joui from "./assets/mangalonga__joui.jpg";
 import mangalonga__arthur from "./assets/mangalonga__arthur.jpg";
@@ -20,26 +20,26 @@ import mangalonga__dante from "./assets/mangalonga__dante.jpg";
 
 export function App() {
   const [moletom, setMoletom] = useState(true);
-  const [camisa, setCamisa] = useState(true);
+  const [camiseta, setCamiseta] = useState(true);
   const [mangalonga, setMangalonga] = useState(true);
 
   const filterMoletom = () => {
     setMoletom(true);
-    setCamisa(false);
+    setCamiseta(false);
     setMangalonga(false);
 
-    if (moletom === true && camisa === false && mangalonga === false) {
-      setCamisa(true);
+    if (moletom === true && camiseta === false && mangalonga === false) {
+      setCamiseta(true);
       setMangalonga(true);
     }
   };
 
-  const filterCamisa = () => {
+  const filterCamiseta = () => {
     setMoletom(false);
-    setCamisa(true);
+    setCamiseta(true);
     setMangalonga(false);
 
-    if (camisa === true && moletom === false && mangalonga === false) {
+    if (camiseta === true && moletom === false && mangalonga === false) {
       setMoletom(true);
       setMangalonga(true);
     }
@@ -47,11 +47,11 @@ export function App() {
 
   const filterMangalonga = () => {
     setMoletom(false);
-    setCamisa(false);
+    setCamiseta(false);
     setMangalonga(true);
 
-    if (mangalonga == true && moletom === false && camisa === false) {
-      setCamisa(true);
+    if (mangalonga == true && moletom === false && camiseta === false) {
+      setCamiseta(true);
       setMoletom(true);
     }
   };
@@ -60,13 +60,22 @@ export function App() {
     <div className="application">
       <div className="photo__galery">
         <div className="filter__area">
-          <button className="filter__btn" onClick={filterCamisa}>
+          <button
+            className={camiseta ? "filter__btn" : "filter__btn disabled__btn"}
+            onClick={filterCamiseta}
+          >
             Camisetas
           </button>
-          <button className="filter__btn" onClick={filterMoletom}>
+          <button
+            className={moletom ? "filter__btn" : "filter__btn disabled__btn"}
+            onClick={filterMoletom}
+          >
             Moletom
           </button>
-          <button className="filter__btn" onClick={filterMangalonga}>
+          <button
+            className={mangalonga ? "filter__btn" : "filter__btn disabled__btn"}
+            onClick={filterMangalonga}
+          >
             Manga Longa
           </button>
         </div>
@@ -92,28 +101,28 @@ export function App() {
             alt=""
           />
           <img
-            className={camisa ? "camisa" : "camisa disabled"}
-            src={camisa__carina}
+            className={camiseta ? "camiseta" : "camiseta disabled"}
+            src={camiseta__carina}
             alt=""
           />
           <img
-            className={camisa ? "camisa" : "camisa disabled"}
-            src={camisa__ordem}
+            className={camiseta ? "camiseta" : "camiseta disabled"}
+            src={camiseta__ordem}
             alt=""
           />
           <img
-            className={camisa ? "camisa" : "camisa disabled"}
-            src={camisa__joui}
+            className={camiseta ? "camiseta" : "camiseta disabled"}
+            src={camiseta__joui}
             alt=""
           />
           <img
-            className={camisa ? "camisa" : "camisa disabled"}
-            src={camisa__arthur}
+            className={camiseta ? "camiseta" : "camiseta disabled"}
+            src={camiseta__arthur}
             alt=""
           />
           <img
-            className={camisa ? "camisa" : "camisa disabled"}
-            src={camisa__mestre}
+            className={camiseta ? "camiseta" : "camiseta disabled"}
+            src={camiseta__mestre}
             alt=""
           />
           <img
